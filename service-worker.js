a8032de6533c04662c41e088328b94206dedd26f
AKQTECH <aprilquioh@gmail.com>
2026-04-06 18:09:04 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pick-me-v1';
+const urlsToCache = [
+  '/PICK-ME-WRITERS-ASST/',
+  '/PICK-ME-WRITERS-ASST/index.html',
+  '/PICK-ME-WRITERS-ASST/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('/PICK-ME-WRITERS-ASST/index.html');
+    })
+  );
+});
