a058fce760ce1f36ca47e827f1231dd927efabce
AKQTECH <aprilquioh@gmail.com>
2026-07-18 04:09:57 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260718o";
+const CACHE_NAME = "pick-me-writers-assistant-20260718p";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
