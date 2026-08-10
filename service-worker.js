3e38cc20108e3946bce37e6242646d640ccf20a3
AKQTECH <aprilquioh@gmail.com>
2026-08-10 14:50:42 -0700

    Simplify companion tab and mobile PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260718r";
+const CACHE_NAME = "pick-me-writers-assistant-20260810b";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
