b3ee2fc03ae0a73b98dd85a5351432438d026bbc
AKQTECH <aprilquioh@gmail.com>
2026-08-10 15:32:19 -0700

    Add companion archive and accurate progress counts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260810b";
+const CACHE_NAME = "pick-me-writers-assistant-20260810c";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
