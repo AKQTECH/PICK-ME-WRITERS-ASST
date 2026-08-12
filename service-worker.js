4ec9c811a4e1c78a416be4c74745165151d11602
AKQTECH <aprilquioh@gmail.com>
2026-08-12 04:21:56 -0700

    Redesign companion as action log

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260811a";
+const CACHE_NAME = "pick-me-writers-assistant-20260812a";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
