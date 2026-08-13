cd147169a0bae27205016a6d561bf7102c91c07f
AKQTECH <aprilquioh@gmail.com>
2026-08-12 21:18:54 -0700

    Simplify companion picker tabs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260812e";
+const CACHE_NAME = "pick-me-writers-assistant-20260812f";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
