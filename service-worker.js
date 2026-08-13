305314ce6338e59f436842fa949e31928a37eabd
AKQTECH <aprilquioh@gmail.com>
2026-08-12 18:25:33 -0700

    Simplify companion scene change log

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260812a";
+const CACHE_NAME = "pick-me-writers-assistant-20260812b";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
