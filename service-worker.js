302e6ef8b07263a63feee6110197bc38606aa08f
AKQTECH <aprilquioh@gmail.com>
2026-08-12 19:37:54 -0700

    Refine companion picker and archive icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260812b";
+const CACHE_NAME = "pick-me-writers-assistant-20260812c";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
