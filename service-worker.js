3afa8ddc7fa9bbee7e61cea3df5dec669ca24ca9
AKQTECH <aprilquioh@gmail.com>
2026-08-12 20:36:21 -0700

    Separate companion ideas inbox

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260812d";
+const CACHE_NAME = "pick-me-writers-assistant-20260812e";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
