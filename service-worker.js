03a6545fa2bad505918e3cbd024e16d3f0f2ab0c
AKQTECH <aprilquioh@gmail.com>
2026-08-14 20:11:22 -0700

    Improve PWA companion mobile layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260813e";
+const CACHE_NAME = "pick-me-writers-assistant-20260814a";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
