9305ac8d82ba3edaca5eb62de8369706e37c4d85
AKQTECH <aprilquioh@gmail.com>
2026-08-15 22:10:41 -0700

    Convert companion scene picker to dropdown

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260815a";
+const CACHE_NAME = "pick-me-writers-assistant-20260815b";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
