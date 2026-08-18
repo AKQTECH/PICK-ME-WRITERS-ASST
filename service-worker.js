b02faaea90765be08b7f3c2caddbe692f191ded1
AKQTECH <aprilquioh@gmail.com>
2026-08-17 22:35:51 -0700

    Update companion mobile editing flow

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260815b";
+const CACHE_NAME = "pick-me-writers-assistant-20260817a";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
