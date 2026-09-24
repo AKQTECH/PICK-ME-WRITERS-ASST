c2ebc7e84c7a3108721daef6c41898cf8f0275f1
AKQTECH <aprilquioh@gmail.com>
2026-09-23 22:15:32 -0700

    Persist Companion change order and editor size

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260923c";
+const CACHE_NAME = "pick-me-writers-assistant-20260923d";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
