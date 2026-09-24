08ff561f56b4604a5af667415e183019f16193d8
AKQTECH <aprilquioh@gmail.com>
2026-09-24 04:57:14 -0700

    Add sortable scene table and protect Dropbox Notion URL

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260923d";
+const CACHE_NAME = "pick-me-writers-assistant-20260924a";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
