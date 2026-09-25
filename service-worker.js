d3902a46921b26df473484c41d846b49673ca896
AKQTECH <aprilquioh@gmail.com>
2026-09-25 01:51:40 -0700

    Preserve scene changes when local storage is full

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260925a";
+const CACHE_NAME = "pick-me-writers-assistant-20260925b";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
