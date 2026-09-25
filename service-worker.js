9e754c116c0fc54f6ce5231fcb2dfe941fcadc22
AKQTECH <aprilquioh@gmail.com>
2026-09-24 21:26:35 -0700

    Streamline Companion navigation and scene editing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260924c";
+const CACHE_NAME = "pick-me-writers-assistant-20260924e";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
