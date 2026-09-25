d69fe824ee4c0e9c9da9a6e31ead7a36a502d115
AKQTECH <aprilquioh@gmail.com>
2026-09-25 15:26:26 -0700

    Polish companion ideas and scene navigation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260925g";
+const CACHE_NAME = "pick-me-writers-assistant-20260925h";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
