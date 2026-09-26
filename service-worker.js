ae941582682fbbb235168988f35f590e063efcd9
AKQTECH <aprilquioh@gmail.com>
2026-09-25 18:31:59 -0700

    Align scene title and auto-size wrapped names

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260925k";
+const CACHE_NAME = "pick-me-writers-assistant-20260925l";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
