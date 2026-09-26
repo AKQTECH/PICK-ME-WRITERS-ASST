8b86891f03ccf3be93fe32f6a0ce03769b727a35
AKQTECH <aprilquioh@gmail.com>
2026-09-25 17:21:16 -0700

    Refine scene overviews and idea presentation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260925h";
+const CACHE_NAME = "pick-me-writers-assistant-20260925i";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
