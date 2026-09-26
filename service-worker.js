79ca67e24d38cf42bdb0988b69516cdbee58fd55
AKQTECH <aprilquioh@gmail.com>
2026-09-25 18:55:07 -0700

    Allow moving existing scenes between sequences

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260925m";
+const CACHE_NAME = "pick-me-writers-assistant-20260925n";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
