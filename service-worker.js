55c7540fa1cc7d0b2102d41e0f7e2419730ebeac
AKQTECH <aprilquioh@gmail.com>
2026-09-25 18:15:10 -0700

    Tighten scene editor header spacing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pick-me-writers-assistant-20260925i";
+const CACHE_NAME = "pick-me-writers-assistant-20260925j";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.jpg"];
 
 self.addEventListener("install", event => {
